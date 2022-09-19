297c3c3790de2dc131453544749daf4de5bbdd37
BlackPearlsDev <proeslros2@gmail.com>
2022-09-19 14:21:17 +0200

    fix product detail on desktop, remove some clg

--- src/Components/Pages/Shop/Detail/Index.jsx
+++ src/Components/Pages/Shop/Detail/Index.jsx
@@ -1,36 +1,40 @@
 import { Link, useParams } from "react-router-dom";
+import { useDispatch, useSelector } from 'react-redux';
+import { setToggle } from '../../../../store/slices/menu.slice';
 
 function Detail({ products }) {
+    const dispatch = useDispatch();
+    const { isToggle } = useSelector((state) => ({...state.menu}));
+
     const { id } = useParams();
-    console.log('id', id);
 
     const product = products.find((product) => product.id === parseInt(id));
-    console.log('product', product);
-
-
-    console.log(`images/${product.image_name}`);
 
     return (
         <main>
-            <h2>Detail du produit</h2>
+            <div className={isToggle ? "overlay" : undefined} onClick={() => dispatch(setToggle(!isToggle))}></div>
+            
+            <section className='mainContent'>
+                <h2>Detail du produit</h2>
 
-            <section className="productDetail">
-                <img src={`/images/${product.image_name}`} alt={product.title} />
-                <h3>{product.title}</h3>
-                <p className="txtContent">{product.description}</p>
+                <section className="productDetail">
+                    <img src={`/images/${product.image_name}`} alt={product.title} />
+                    <h3>{product.title}</h3>
+                    <p className="txtContent">{product.description}</p>
 
-                <div className="txtPriceQuantity">
-                    <p><span className="txtBlue">{product.price}</span>€</p>
-                    <p>Disponible: <span className="txtBlue">{product.quantityInStock}</span></p>
-                </div>
+                    <div className="txtPriceQuantity">
+                        <p><span className="txtBlue">{product.price}</span>€</p>
+                        <p>Disponible: <span className="txtBlue">{product.quantityInStock}</span></p>
+                    </div>
 
-                <button className='btnAddCart btnCartDetail'>Ajouter au panier</button>
+                    <button className='btnAddCart btnCartDetail'>Ajouter au panier</button>
+                </section>
+
+                <aside>
+                    <p>Vous pouvez ajoutez ce produit à votre panier ou bien retourner consulter l'ensemble de nos produits</p>
+                    <Link to="/product" className="txtLinkBlue">Retour aux produits</Link>
+                </aside>
             </section>
-
-            <aside>
-                <p>Vous pouvez ajoutez ce produit à votre panier ou bien retourner consulter l'ensemble de nos produits</p>
-                <Link to="/product" className="txtLinkBlue">Retour aux produits</Link>
-            </aside>
         </main>
     )
 }
